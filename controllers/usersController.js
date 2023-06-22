13d9c8f741e830a53aec750b4fb0ae2374889e63
ge-arty <george.arutinyan@gmail.com>
2023-06-22 16:46:34 +0400

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,5 @@
 const User = require("../models/userSchema");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const generateToken = require("../configs/JWTtoken");
 
 // Register User Api
