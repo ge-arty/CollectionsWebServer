e7fa50b86d64a3687db60a573b9bea27832d49c2
ge-arty <george.arutinyan@gmail.com>
2023-06-28 15:11:31 +0400

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -112,10 +112,9 @@ const userLogout = expressAsyncHandler(async (req, res) => {
 const createCollection = expressAsyncHandler(async (req, res) => {
   try {
     const { userId, collectionData } = req.body;
-    console.log(req.body);
-    if (!userId || !collectionData) {
-      return res.status(400).json({ error: "Not enough info about User!" });
-    }
+    // if (!userId || !collectionData) {
+    //   return res.status(400).json({ error: "Not enough info about User!" });
+    // }
     const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ error: "User not found!" });
