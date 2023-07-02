70b94dc6f467b35036e3d38ef20932327f670b22
ge-arty <george.arutinyan@gmail.com>
2023-07-02 16:16:25 +0400

    Update cloudinary.js

--- configs/cloudinary.js
+++ configs/cloudinary.js
@@ -2,7 +2,7 @@ const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: "dbfpdhvwk",
-  api_key: process.env.CLOUDINARY_API_KEY,
+  api_key: "94uudFSwJRlq7E7TEygxUA1S3t8",
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
