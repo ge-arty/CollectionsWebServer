2e8e36d1fc5934292058987616703041515da041
ge-arty <george.arutinyan@gmail.com>
2023-07-18 14:38:00 +0400

    extended request size

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const cors = require("cors");
 const PORT = process.env.PORT;
 // Set up Express.js app
 const app = express();
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
 app.use(cors());
 
